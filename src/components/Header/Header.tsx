bdf2b491692372bce307da26b55b2c6bf570dcf6
Camila Paladines <camilapaladines27@gmail.com>
2023-02-11 14:14:07 -0500

    Add header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import './Header.scss';
-import Logo from './assets/logo.svg';
+import Logo from '../../assets/logo.svg';
+import ArrowDown from '../../assets/icons/arrow-down.svg';
 import DesktopHeaderImage from '../../assets/desktop/header.jpg';
 
 export default function Header() {
 	return (
 		<div className='header'>
 			<img src={DesktopHeaderImage} alt='' />
+			<div>
+				<img src={Logo} alt='' className='logo' />
+				<ul>
+					<li>
+						<a href=''>About</a>
+					</li>
+					<li>
+						<a href=''>Services</a>
+					</li>
+					<li>
+						<a href=''>Projects</a>
+					</li>
+					<li>
+						<button>CONTACT</button>
+					</li>
+				</ul>
+			</div>
 			<h1>WE ARE CREATIVES</h1>
+			<img src={ArrowDown} alt='' className='arrow-down' />
 		</div>
 	);
 }
