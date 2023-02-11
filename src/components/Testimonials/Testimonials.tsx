3f66c27cb6b61b1d2f57cd1d3ece9b484121069a
Camila Paladines <camilapaladines27@gmail.com>
2023-02-11 00:49:38 -0500

    Added testimolials colors

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -7,27 +7,35 @@ import JennieProfile from '../../assets/profiles/jennie.jpg';
 export default function Testimonials() {
 	return (
 		<div className='testimonials'>
-			<h2>Client Testimonials</h2>
+			<h2>CLIENT TESTIMONIALS</h2>
 			<ul>
 				<li>
 					<img src={EmilyProfile} alt='' />
-					<p>We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
-					<p>Emily R.</p>
-					<p>Marketing Director</p>
+					<p className='comment'>
+						We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.
+					</p>
+					<div>
+						<p className='name'>Emily R.</p>
+						<p className='role'>Marketing Director</p>
+					</div>
 				</li>
 				<li>
 					<img src={ThomasProfile} alt='' />
-					<p>
+					<p className='comment'>
 						Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.
 					</p>
-					<p>Thomas S.</p>
-					<p>Chief Operating Officer</p>
+					<div>
+						<p className='name'>Thomas S.</p>
+						<p className='role'>Chief Operating Officer</p>
+					</div>
 				</li>
 				<li>
 					<img src={JennieProfile} alt='' />
-					<p>Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!</p>
-					<p>Jennie F.</p>
-					<p>Business Owner</p>
+					<p className='comment'>Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!</p>
+					<div>
+						<p className='name'>Jennie F.</p>
+						<p className='role'>Business Owner</p>
+					</div>
 				</li>
 			</ul>
 		</div>
