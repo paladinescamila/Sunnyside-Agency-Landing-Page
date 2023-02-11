78377f680ec4a19c4018038aff01f38d0103183c
Camila Paladines <camilapaladines27@gmail.com>
2023-02-11 16:02:31 -0500

    Fixed responsive bugs

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -14,28 +14,22 @@ export default function Testimonials() {
 					<p className='comment'>
 						We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.
 					</p>
-					<div>
-						<p className='name'>Emily R.</p>
-						<p className='role'>Marketing Director</p>
-					</div>
+					<p className='name'>Emily R.</p>
+					<p className='role'>Marketing Director</p>
 				</li>
 				<li>
 					<img src={ThomasProfile} alt='' />
 					<p className='comment'>
 						Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.
 					</p>
-					<div>
-						<p className='name'>Thomas S.</p>
-						<p className='role'>Chief Operating Officer</p>
-					</div>
+					<p className='name'>Thomas S.</p>
+					<p className='role'>Chief Operating Officer</p>
 				</li>
 				<li>
 					<img src={JennieProfile} alt='' />
 					<p className='comment'>Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!</p>
-					<div>
-						<p className='name'>Jennie F.</p>
-						<p className='role'>Business Owner</p>
-					</div>
+					<p className='name'>Jennie F.</p>
+					<p className='role'>Business Owner</p>
 				</li>
 			</ul>
 		</div>
